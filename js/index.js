16d9323c2597bfe80de5d4ef91408fb622adefcc
Shaktyka <k-i-s-u-n-j-a@list.ru>
2022-03-27 13:07:22 +0500

    Добавляет overlay, переделывает открытие-закрытие модалки

--- js/index.js
+++ js/index.js
@@ -3,30 +3,25 @@ const headerRequestBtn = document.querySelector('.header__request');
 const heroRequestBtn = document.querySelector('.hero__request');
 const sendRequestBtn = document.querySelector('.send-request__btn');
 
-const modal = document.querySelector('.modal');
-const sendForm = modal.querySelector('.send__form');
+const overlay = document.querySelector('.overlay');
+const sendForm = overlay.querySelector('.send__form');
 
 // Открывает модалку:
 const openModal = () => {
-    modal.classList.add('show');
+    overlay.classList.add('show');
     
 };
 
 // Закрывает модалку:
 const closeModal = () => {
-    modal.classList.remove('show');
+    sendForm.reset();
+    overlay.classList.remove('show');
 };
 
 // Обработчики:
 const requestBtnClickHandler = (evt) => {
     evt.preventDefault();
-    const isOpened = modal.classList.contains('show');
-
-    if (isOpened) {
-        closeModal();
-    } else {
-        openModal();
-    }
+    openModal();
 };
 
 // Отправка формы:
@@ -36,8 +31,14 @@ const sendFormSubmitHandler = () => {
     closeModal();
 };
 
+// Клик на оверлей:
+const overlayClickHandler = () => {
+    closeModal();
+};
+
 // Подключение обработчиков:
 sendForm.addEventListener('submit', sendFormSubmitHandler);
 headerRequestBtn.addEventListener('click', requestBtnClickHandler);
 heroRequestBtn.addEventListener('click', requestBtnClickHandler);
 sendRequestBtn.addEventListener('click', requestBtnClickHandler);
+overlay.addEventListener('click', overlayClickHandler);
