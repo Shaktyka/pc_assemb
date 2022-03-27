74bcf5054c7b64305900eed356017ec01d8549ea
Shaktyka <k-i-s-u-n-j-a@list.ru>
2022-03-27 12:43:58 +0500

    Реализует открытие-закрытие модалки. Мелкие правки стилей

--- js/index.js
+++ js/index.js
@@ -1,8 +1,43 @@
+// Переменные:
 const headerRequestBtn = document.querySelector('.header__request');
+const heroRequestBtn = document.querySelector('.hero__request');
+const sendRequestBtn = document.querySelector('.send-request__btn');
 
-const headerRequestBtnHandler = (evt) => {
-    evt.preventDefault();
-    console.log(1);
+const modal = document.querySelector('.modal');
+const sendForm = modal.querySelector('.send__form');
+
+// Открывает модалку:
+const openModal = () => {
+    modal.classList.add('show');
+    
 };
 
-headerRequestBtn.addEventListener('click', headerRequestBtnHandler);
+// Закрывает модалку:
+const closeModal = () => {
+    modal.classList.remove('show');
+};
+
+// Обработчики:
+const requestBtnClickHandler = (evt) => {
+    evt.preventDefault();
+    const isOpened = modal.classList.contains('show');
+
+    if (isOpened) {
+        closeModal();
+    } else {
+        openModal();
+    }
+};
+
+// Отправка формы:
+const sendFormSubmitHandler = () => {
+    evt.preventDefault();
+    // Валидация данных формы
+    closeModal();
+};
+
+// Подключение обработчиков:
+sendForm.addEventListener('submit', sendFormSubmitHandler);
+headerRequestBtn.addEventListener('click', requestBtnClickHandler);
+heroRequestBtn.addEventListener('click', requestBtnClickHandler);
+sendRequestBtn.addEventListener('click', requestBtnClickHandler);
